7dfecf343dac5d17a4179c6c9b9c6e83f9b00413
Aman Raj <135144595+Amanraj0149@users.noreply.github.com>
2024-11-12 22:26:29 +0530

    Create RemoveDuplicateUsingset.cpp

---
+++ RemoveDuplicateUsingset.cpp
@@ -0,0 +1,17 @@
+Node  RemoveDuplicate(Node* head){
+  unordered_set<int> st;
+Node* curr=head;
+Node* prev=nullptr;
+  while(curr!=NULL){ 
+     if(st.find(curr->data)!=st.end()){ // duplicate remove kardo;
+       prev->next=curr->next;
+       delete curr;
+     }
+    else{
+         st.insert(curr->data); 
+       prev=curr;
+    }
+  }
+  curr=prev->next;
+ return curr;
+}
